050c4c92629ad1c4315ef9fbaf6b5e2e8122e610
sayooj anil <sayoojanil977@gmail.com>
2026-08-02 16:31:12 +0530

    s

---
+++ config/googleOAuth.js
@@ -0,0 +1,11 @@
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
+const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI || "http://localhost:5000/api/auth/google/callback";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+module.exports = {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_REDIRECT_URI,
+  FRONTEND_URL,
+};
